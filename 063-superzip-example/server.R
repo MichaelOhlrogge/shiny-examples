285ec58b9c05adf4acdbf537f38eec0d8c51a953
Ohlrogge <ohlrogge@gmail.com>
2016-09-08 12:47:56 -0700

    all zips

--- 063-superzip-example/server.R
+++ 063-superzip-example/server.R
@@ -6,10 +6,10 @@ library(dplyr)
 
 # Leaflet bindings are a bit slow; for now we'll just sample to compensate
 set.seed(100)
-zipdata <- allzips[sample.int(nrow(allzips), 10000),]
+# zipdata <- allzips[sample.int(nrow(allzips), 10000),]
 # By ordering by Delinquencies, we ensure that the (comparatively rare) SuperZIPs
 # will be drawn last and thus be easier to see
-zipdata <- zipdata[order(zipdata$Delinquencies),]
+# zipdata <- zipdata[order(zipdata$Foreclosures),]
 
 function(input, output, session) {
 
